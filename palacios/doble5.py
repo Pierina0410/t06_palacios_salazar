1fc341bc4adaf4ba83eb380f9cca9d9732872a90
Pierina0410 <57008210+pierina0410@users.noreply.github.com>
2019-11-17 12:57:33 -0500

    haciendo la mitad de los condicionales dobles

---
+++ palacios/doble5.py
@@ -0,0 +1,37 @@
+import os
+#declarar variables
+cliente,vendedor,jabones,pu="","",0.0,0.0
+#imput
+cliente= os.sys.argv[1]
+vendedor=os.sys.argv[2]
+jabones=float(os.sys.argv[3])
+pu=float (os.sys.argv[4])
+
+#procesing
+total=(jabones*pu)
+#verificador
+por_compras=(total>600)
+comprador_compulsivo= (total>800)
+
+#ouput
+
+print("  BOLETA ELECTRONICA     ")
+print("   RIPLEY        ")
+print(" cliente:",cliente)
+print("vendedor:",vendedor)
+print("fecha=8/11/2019  Hora :8:50pm ")
+print("jabones:",jabones ,"unidades")
+print("p.u:",pu)
+print("TOTAL:",total)
+print("#### GRACIAS POR SU COMPRA ###")
+print("##############################")
+#condicional doble
+#si es comprador recurrente otorgar 20% de descuento
+if (por_compras==True):
+    print("GANASTE UN DESCUENTO DEL 20% ")
+#si fuiste el que más compro en el año te 1 caja de jabones
+if (comprador_compulsivo==True):
+    print("FELICIDADES GANASTE 1 CAJA DE JABONES")
+
+
+
